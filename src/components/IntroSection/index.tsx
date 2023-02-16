25fcccf6cb0e13340c7f2a3fd1a092ed93002bbd
arjpeg <araoudupi@gmail.com>
2023-02-15 18:38:25 -0600

    changes?

---
+++ src/components/IntroSection/index.tsx
@@ -0,0 +1,9 @@
+import "./introsection.css";
+
+export default function IntroSection() {
+  return (
+    <div className="intro-section">
+      <h1 className="page-title">Haxiom</h1>
+    </div>
+  );
+}
