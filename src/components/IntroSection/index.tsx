9dfb667225bf0d53423beebe3a326fe97afe8e59
Sriram Polineni <68199583+spol-29@users.noreply.github.com>
2023-02-17 14:31:42 -0600

    fixed typo

--- src/components/IntroSection/index.tsx
+++ src/components/IntroSection/index.tsx
@@ -21,7 +21,7 @@ export default function IntroSection() {
         <br /> Dev
       </h1>
       <p className="page-description">
-        A tutoring service to help you gain a{" "}
+        A tutoring service that help's you gain a{" "}
         <span className="special-highlight">passion for learning</span>
       </p>
     </div>
