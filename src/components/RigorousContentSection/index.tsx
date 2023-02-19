09f4950c8f891a5f540e6d250ee7636a82863e2b
ARandomPenguin <103593185+ARandomPenguin@users.noreply.github.com>
2023-02-19 15:51:13 +0000

    jghcghjk

--- src/components/RigorousContentSection/index.tsx
+++ src/components/RigorousContentSection/index.tsx
@@ -26,7 +26,7 @@ export default function RigorousContentSection() {
       <p className="learn-description">
         With our rigorous content, we focus on teaching students the art of
         learning and problem solving. We teach students how to think {" "}
-        <span className="special-highlight">critically</span> and solve problems in a way that sticks. We also provide our students
+        <span className="special-highlight">critically </span> and solve problems in a way that sticks. We also provide our students
         with abundant resources to understand {" "}
         <span className="special-highlight">high-quality</span> videos and homework.
       </p>
